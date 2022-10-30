ba78cff161f11e375fe7682fd6ff8d0d4d2e4846
Edward Iakab <edwardiakab@yahoo.com>
2022-10-30 14:46:01 +0200

    Add files via command line

--- Year 2 - Sem 1/Advanced Programming Methods/Labs/A1/A1/src/Model/Entity.java
+++
@@ -1,7 +0,0 @@
-package Model;
-
-public interface Entity {
-	public int getRepairCost();
-
-	public String toString();
-}
--- Year 2 - Sem 1/Advanced Programming Methods/Labs/Lab1-Class/Lab1-Class/src/Class.java
+++
@@ -1,9 +0,0 @@
-public class Class {
-	public Class() {
-		System.out.println("Parent class");
-	}
-
-	int add(int x, int y) {
-		return x + y;
-	}
-}
